1f279265b271d719ab2ac10182c25d70c10cb987
rstaats <rstaats@outlook.com>
2016-10-01 11:20:43 -0700

    updated script
    Added Comments to each section, and parameterized the version of Fedora

--- fedora24_broadcom_wl_install.sh
+++ fedora24_broadcom_wl_install.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
-wget http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install rpmfusion-nonfree-release-24.noarch.rpm -y
+
+#Set Version for Fedora
+FedoraVers=$(rpm -E %fedora)
+
+#Download and install RPM fusion repo
+wget http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FedoraVers.noarch.rpm
+dnf install rpmfusion-nonfree-release-$FedoraVers.noarch.rpm -y
+
+#Clean yum metadata install adkmod-wl and kernel-devel
 dnf clean all -y
 dnf install akmod-wl -y 
 dnf install kernel-devel -y
+
+#Force Broadcom driver and modprobing kernel
 akmods --force
 modprobe wl
+
+#runs iwconfig (wireless equivalent of ifconfig) and restarts NetworkManager
 iwconfig
 systemctl restart NetworkManager
