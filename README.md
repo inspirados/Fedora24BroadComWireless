3d02fcda4d68e5998a373f7724a6c45bfc4ab810
Richard Staats <rstaats@rs-e6230.rshome.local>
2016-09-30 22:09:02 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Fedora24BroadComWireless
 Install Wireless Drivers for Fedora 24 on Broadcom
+
+Instructions:
+-clone repo
+-chmod 700
+-sudobash fedora24_broadcom_wl_install.sh
+
+Tested Devices:
+Tested on Dell Latitude E 6xxx series Laptops equipped with Broadcom BCM43228 802.11a/b/g/n
---
+++ fedora24_broadcom_wl_install.sh
@@ -0,0 +1,9 @@
+wget http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install rpmfusion-nonfree-release-24.noarch.rpm -y
+dnf clean all -y
+dnf install akmod-wl -y 
+dnf install kernel-devel -y
+akmods --force
+modprobe wl
+iwconfig
+systemctl restart NetworkManager
